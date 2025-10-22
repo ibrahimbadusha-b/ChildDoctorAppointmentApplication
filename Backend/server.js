048c936543f605d823dba3000e28284002441c5f
ibrahimbadusha-b <ibrahimbadusha2602>
2025-10-23 01:55:55 +0530

    add routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,32 @@
 const express = require('express');
-const app=express();
+const app = express();
 
-const path =require("path");
-const dotenv =require("dotenv");
+const path = require("path");
+const dotenv = require("dotenv");
 const databaseConnection = require('./config/Dbconnection');
-const User=require("./routes/UserRoute");
-const cors=require("cors");
+const User = require("./routes/UserRoute");
+const cors = require("cors");
 const PORT = process.env.PORT || 2000;
 
-dotenv.config({path:path.join(__dirname,"config","config.env")});
+dotenv.config({ path: path.join(__dirname, "config", "config.env") });
 
 databaseConnection();
- app.use(express.json())
- app.use(cors())
-app.get('/',(req,res,next)=>{
- res.send("<h1>Hello Mapla</h1>")}
-       )
-app.use('/api/users',User);
-app.listen(PORT ||2000,()=>{
-console.log(`Server Created SuceessFully ${process.env.PORT}`);
+app.use(express.json())
+app.use(cors())
+app.get('/', (req, res, next) => {
+      res.send("<h1>Hello Mapla</h1>")
+}
+)
+app.get('/date', (res, res, next) => {
+      const date = new Date.now();
+      res.send(date)
+})
+
+app.get("/realnews",(res,res,next)=>{
+      res.send("Tommarrow had a huge rain alert for chennai")
+})
+app.use('/api/users', User);
+app.listen(PORT || 2000, () => {
+      console.log(`Server Created SuceessFully ${process.env.PORT}`);
 
 })
