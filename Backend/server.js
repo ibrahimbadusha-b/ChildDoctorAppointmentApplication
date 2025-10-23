5d3287ce946a764f01598751858dec85a91819e0
ibrahimbadusha-b <ibrahimbadusha2602@gmail.com>
2025-10-23 18:47:47 +0530

    Update PORT

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,7 @@ app.get("/realnews",(req,res,next)=>{
       res.send("Tommarrow had a huge rain alert for chennai")
 })
 app.use('/api/users', User);
- app.listen(PORT, () => {
+ app.listen(PORT,'0.0.0.0', () => {
       console.log(`Server Created SuceessFully ${process.env.PORT}`);
 
  })
